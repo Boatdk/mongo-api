c92ca2bc98924c8f5065d2fadd4aa2ef30e1b2d5
Boatdk <boatz.dk@gmail.com>
2019-10-21 17:08:31 +0700

    add deposit&withdraw query

--- routes/summary.js
+++ routes/summary.js
@@ -5,22 +5,46 @@ const db = require('../utils/query')
 router.route('/v0.1/summary')
 
   .get((req, res) => {
-    var totalDeposit_staff = `SELECT SUM(amount_current) AS total_joker_deposit, COUNT(*) AS total_deposit 
+    const totalDeposit_staff = `SELECT SUM(amount_current) AS total_joker_deposit, COUNT(*) AS total_deposit 
     FROM deposit_log WHERE agent = 'Nikigame' AND status = '1'
     AND (create_date BETWEEN '".date("Y-m-d")." 00:00:00' AND '".date("Y-m-d")." 23:59:59')
     AND create_by = 'staff'`
 
-    var totalDeposit_system = `SELECT SUM(amount_current) AS total_joker_deposit, COUNT(*) AS total_deposit 
+    const totalDeposit_system = `SELECT SUM(amount_current) AS total_joker_deposit, COUNT(*) AS total_deposit 
     FROM deposit_log WHERE agent = 'Nikigame' AND status = '1'
     AND (create_date BETWEEN '".date("Y-m-d")." 00:00:00' AND '".date("Y-m-d")." 23:59:59')
     AND create_by = 'system'`
 
-    db(totalDeposit_staff).then(staff => {
-      db(totalDeposit_system).then(system => {
-        
-        console.log("staff => " + staff[0].total_joker_deposit + "  ///  " + system[0].total_joker_deposit)
+    const totalWithdraw_staff = `SELECT SUM(amount) AS total_joker_withdraw, COUNT(*) AS total_withdraw
+    FROM withdraw_log WHERE agent = 'wallet' AND status = '1'
+    AND (create_date BETWEEN '".date("Y-m-d")." 00:00:00' AND '".date("Y-m-d")." 23:59:59')
+    AND create_by = 'staff'`
+
+    const totalWithdraw_system = `SELECT SUM(amount) AS total_joker_withdraw, COUNT(*) AS total_withdraw 
+    FROM withdraw_log WHERE agent = 'wallet' AND status = '1'
+    AND (create_date BETWEEN '".date("Y-m-d")." 00:00:00' AND '".date("Y-m-d")." 23:59:59')
+    AND create_by = 'system'`
+
+    db(totalDeposit_staff).then(depositStaff => {
+      db(totalDeposit_system).then(depositSystem => {
+        db(totalWithdraw_staff).then(withdrawStaff => {
+          db(totalWithdraw_system).then(withdrawSystem => {
+            if(depositStaff && depositSystem && withdrawStaff && withdrawSystem){
+              var deposit = parseFloat(depositStaff[0].total_joker_deposit) + parseFloat(depositSystem[0].total_joker_deposit)
+              var countDeposit = parseInt(depositStaff[0].total_deposit) + parseInt(depositSystem[0].total_deposit)
+              var withdraw = parseFloat(withdrawStaff[0].total_joker_withdraw) + parseFloat(withdrawSystem[0].total_joker_withdraw)
+              var countWithdraw = parseInt(withdrawStaff[0].total_withdraw) + parseInt(withdrawSystem[0].total_withdraw)
+              res.json({
+                deposit: deposit,
+                count_deposit: countDeposit,
+                withdraw: withdraw,
+                count_withdraw: countWithdraw
+              })
+            }
+          })
+        })
       })
     })
   })
 
-  module.exports = router
+module.exports = router
